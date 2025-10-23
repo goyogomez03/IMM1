093886c80b7a59d8156d95dbb66e5dc6bc4a5162
goyogomez03 <goyogomez03@gmail.com>
2025-10-23 00:29:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,9 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.subheader("Una pequeña historia")
+st.write('Wall-E** cuenta la historia de un pequeño robot recolector de basura que vive solo en una Tierra abandonada y cubierta de desechos. Durante siglos ha limpiado el planeta mientras desarrolla una personalidad curiosa y sensible. Su rutina cambia cuando conoce a **EVE**, una moderna robot exploradora enviada para buscar signos de vida. Juntos emprenden una aventura que los lleva al espacio, donde descubren a los humanos viviendo cómodamente en una nave y olvidados de su hogar. Gracias a su valentía y ternura, Wall-E inspira a la humanidad a regresar a la Tierra y cuidarla de nuevo.
+'
         
         )
            
